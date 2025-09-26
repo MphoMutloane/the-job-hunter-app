8f18107aa3ba20daa2d6d59a73d0defb671bf5e3
mpho <mphomutloane.mm@gmail.com>
2025-09-26 16:34:36 +0200

    Complleted setting up mailtrap verification code email

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,7 +1,7 @@
 import { mailtrapClient, sender } from "./mailtrapConfig.js";
 import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
 
-export const sendVerificationEmail = async (email, verificationToken) => {
+const sendVerificationEmail = async (email, verificationToken) => {
 	const recipient = [{ email }];
 
 	try {
@@ -20,3 +20,27 @@ export const sendVerificationEmail = async (email, verificationToken) => {
 		throw new Error(`Error sending verification email: ${error}`);
 	}
 };
+
+const sendWelcomeEmail = async (email, name) => {
+	const recipient = [{ email }];
+
+	try {
+		const response = await mailtrapClient.send({
+			from: sender,
+			to: recipient,
+			template_uuid: "7ef1cbd6-e7ba-476e-9e82-fddd9154cb68",
+			template_variables: {
+				company_info_name: "The Job Hunter App",
+				name: name,
+			},
+		});
+
+		console.log("Welcome email sent successfully", response);
+	} catch (error) {
+		console.error(`Error sending welcome email`, error);
+
+		throw new Error(`Error sending welcome email: ${error}`);
+	}
+};
+
+export { sendVerificationEmail, sendWelcomeEmail };
